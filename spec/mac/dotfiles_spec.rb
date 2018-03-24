39229ad3b2ed31c5a572dd47545a6c487ba6584f
Takuma Homma <nagomimatcha@gmail.com>
2018-03-25 02:57:29 +0900

    spec for dotfiles

---
+++ spec/mac/dotfiles_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe file File.expand_path('dotfiles', '~') do
+  it { is_expected.to be_directory }
+end
+
+%w(.vim .emacs.d .config/fish .config/fish/functions .config/powerline .atom).each do |dir|
+  describe file File.expand_path(dir, '~') do
+    it { is_expected.to be_directory }
+  end
+end
+
+%w(.gitconfig .vimrc .zshrc .tmux.conf .config/fish/config.fish).each do |file|
+  describe file File.expand_path(file, '~') do
+    it { is_expected.to be_file }
+  end
+end

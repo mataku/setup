b9961608ee1cbce2f35db106ad69da57a8a6ce51
Takuma Homma <nagomimatcha@gmail.com>
2018-03-25 02:20:01 +0900

    check whether emacs installed by homebrew and vim with option

--- spec/mac/brew_spec.rb
+++ spec/mac/brew_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe file '/usr/local/bin/brew' do
-  it { should be_file }
-  it { should be_executable }
+  it { is_expected.to be_file }
+  it { is_expected.to be_executable }
+end
+
+describe command '/usr/local/bin/emacs' do
+  it { is_expected.to be_file }
+  it { is_expected.to be_executable }
+end
+
+describe command 'vim --version | grep +lua' do
+  its(:exit_status) { is_expected.to eq 0 }
 end

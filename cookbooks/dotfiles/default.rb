e948f0381e68b7a360528adbb8a782296064b0dd
Takuma Homma <nagomimatcha@gmail.com>
2018-03-25 18:16:23 +0900

    Move github repository to ~/github.com

--- cookbooks/dotfiles/default.rb
+++ cookbooks/dotfiles/default.rb
@@ -1,13 +1,13 @@
-%w(.atom .config).each do |dir|
+%w(.atom .config github.com).each do |dir|
   execute 'Create config dir' do
     command "mkdir -p ~/#{dir}"
   end
 end
 
-git File.expand_path('~/dotfiles') do
+git File.expand_path('~/github.com/dotfiles') do
   repository 'https://github.com/mataku/dotfiles.git'
 end
 
 execute 'Create symbolic links' do
-  command '/bin/bash ~/dotfiles/install.sh'
+  command '/bin/bash ~/github.com/dotfiles/install.sh'
 end

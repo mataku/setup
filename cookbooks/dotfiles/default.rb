92c8928a2dd1699689063905be0b818e56d06a11
Takuma Homma <nagomimatcha@gmail.com>
2018-07-29 00:23:06 +0900

    Root path to ~/src

--- cookbooks/dotfiles/default.rb
+++ cookbooks/dotfiles/default.rb
@@ -1,13 +1,13 @@
-%w(.atom .config github.com).each do |dir|
+%w(.atom .config src).each do |dir|
   execute 'Create config dir' do
     command "mkdir -p ~/#{dir}"
   end
 end
 
-git File.expand_path('~/github.com/dotfiles') do
+git File.expand_path('~/src/github.com/dotfiles') do
   repository 'https://github.com/mataku/dotfiles.git'
 end
 
 execute 'Create symbolic links' do
-  command '/bin/bash ~/github.com/dotfiles/install.sh'
+  command '/bin/bash ~/src/github.com/dotfiles/install.sh'
 end

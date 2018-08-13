fc7a863b1e53ff901b2a0c9f5c61976d735a8a18
Takuma Homma <nagomimatcha@gmail.com>
2018-08-13 17:17:06 +0900

    command with execute block

--- cookbooks/after/default.rb
+++ cookbooks/after/default.rb
@@ -1,6 +1,8 @@
 require 'yaml'
 
-command 'gem i terminal-notifier --no-doc'
+execute 'Install terminal-notifier' do
+  command 'gem i terminal-notifier --no-doc'
+end
 
 node['gui']['packages'].each do |app|
   execute 'List GUI applications to install' do

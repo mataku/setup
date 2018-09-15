89c2f90d16098cd2c2df1d1f6cebaf049c5e9823
Takuma Homma <nagomimatcha@gmail.com>
2018-09-16 02:27:58 +0900

    Normalized name

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -1,7 +1,6 @@
-require 'yaml'
-
 execute 'Install homebrew' do
   command '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+  not_if 'test $(which /usr/local/bin/brew)'
 end
 
 node['homebrew']['repositories'].each do |repo|

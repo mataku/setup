53bbf6fb229d0e4ee3dd67566a05b03d8dc8ff90
Takuma Homma <nagomimatcha@gmail.com>
2018-09-16 01:54:59 +0900

    Use mitamae instead of itamae

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -ex
-gem i bundler --no-doc
-bundle install -j2 --path vendor/bundle
-bundle exec itamae local recipes/all.rb -y nodes/mac.yml
+MITAMAE_VERSION="1.6.3"
+
+curl -Lo mitamae-x86_64-darwin.tar.gz https://github.com/k0kubun/mitamae/releases/download/v${MITAMAE_VERSION}/mitamae-x86_64-darwin.tar.gz
+tar -zxf mitamae-x86_64-darwin.tar.gz
+chmod +x mitamae-x86_64-darwin
+mkdir bin && ln -sf mitamae-x86_64-darwin.tar.gz bin/mitamae
+
+echo y | bin/mitamae local recipes/all.rb -y nodes/mac.yml

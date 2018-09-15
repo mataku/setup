1f361b26c5c719efe3126195ca0f9bbcd35a9aab
Takuma Homma <nagomimatcha@gmail.com>
2018-09-16 02:23:16 +0900

    Do `xcode-select --install` if only setup

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -ex
 MITAMAE_VERSION="1.6.3"
 
+if [ ! $CI ]; then
+  xcode-select --install
+fi
+
 curl -Lo mitamae-x86_64-darwin.tar.gz https://github.com/k0kubun/mitamae/releases/download/v${MITAMAE_VERSION}/mitamae-x86_64-darwin.tar.gz
 tar -zxf mitamae-x86_64-darwin.tar.gz
+rm mitamae-x86_64-darwin.tar.gz
 chmod +x mitamae-x86_64-darwin
-mkdir bin && ln -sf mitamae-x86_64-darwin.tar.gz bin/mitamae
-
+mkdir bin && mv mitamae-x86_64-darwin bin/mitamae
 echo y | bin/mitamae local recipes/all.rb -y nodes/mac.yml

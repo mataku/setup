0fd1b73f264a832e6b922c142f1bb2dd051e1b73
Takuma Homma <nagomimatcha@gmail.com>
2019-02-25 23:38:39 +0900

    Install dependencies via Homebrew using Brewfile

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -3,20 +3,6 @@ execute 'Install homebrew' do
   not_if 'test $(which /usr/local/bin/brew)'
 end
 
-node['homebrew']['repositories'].each do |repo|
-  execute 'Tap repositories' do
-    command "brew tap #{repo}"
-  end
-end
-
-node['homebrew']['packages'].each do |package|
-  execute "Install #{package}" do
-    command "brew install #{package}"
-  end
-end
-
-node['homebrew']['cask_packages'].each do |package|
-  execute "Install #{package} via cask" do
-    command "brew cask install #{package}"
-  end
+execute 'Install dependencies' do
+  command 'brew bundle'
 end
--- spec/mac/brew_spec.rb
+++ spec/mac/brew_spec.rb
@@ -9,7 +9,3 @@ describe file '/usr/local/bin/emacs' do
   it { is_expected.to be_file }
   it { is_expected.to be_executable }
 end
-
-describe command 'vim --version | grep +lua' do
-  its(:exit_status) { is_expected.to eq 0 }
-end

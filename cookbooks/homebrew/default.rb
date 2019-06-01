7bdf6a03bdcfcfccbc8e2b6e588b60e4069162bd
Takuma Homma <nagomimatcha@gmail.com>
2019-06-01 20:43:51 +0900

    verbose

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -5,6 +5,12 @@ end
 
 execute 'Install dependencies' do
   command 'brew bundle'
+  not_if { ENV['CI'] == "true" }
+end
+
+execute 'Install dependencies with debug logs' do
+  command 'brew bundle --verbose'
+  not_if { ENV['CI'] == "false" }
 end
 
 execute 'Install dependencies via mas' do

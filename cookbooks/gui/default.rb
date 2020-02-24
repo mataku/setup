5f5b8fa5ccb32eeb59b316da52d2eb82457f36e7
Takuma Homma <nagomimatcha@gmail.com>
2020-02-24 19:35:24 +0900

    Kick terminal-notifier

--- cookbooks/gui/default.rb
+++ cookbooks/gui/default.rb
@@ -1,7 +1,3 @@
-execute 'Install terminal-notifier' do
-  command 'gem i terminal-notifier --no-doc'
-end
-
 node['gui']['packages'].each do |app|
   execute 'List GUI applications to install' do
     command "echo #{app}"

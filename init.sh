1d4fe0d702cd2ccb63cd030f37b40508b55a0869
Takuma Homma <nagomimatcha@gmail.com>
2020-02-24 19:26:51 +0900

    Where is source...

--- init.sh
+++ init.sh
@@ -5,9 +5,11 @@ if [ ! $CI ]; then
   xcode-select --install
 fi
 
+mkdir -p ~/src/github.com/mataku
+git clone https://github.com/mataku/mac-setup ~/src/github.com/mataku/mac-setup
 curl -Lo mitamae-x86_64-darwin.tar.gz https://github.com/k0kubun/mitamae/releases/download/v${MITAMAE_VERSION}/mitamae-x86_64-darwin.tar.gz
 tar -zxf mitamae-x86_64-darwin.tar.gz
 rm mitamae-x86_64-darwin.tar.gz
 chmod +x mitamae-x86_64-darwin
 mkdir bin && mv mitamae-x86_64-darwin bin/mitamae
-echo y | bin/mitamae local recipes/all.rb -y nodes/mac.yml --log-level=DEBUG
+echo y | bin/mitamae local ~/src/github.com/mataku/mac-setup/recipes/all.rb -y ~/src/github.com/mataku/mac-setup/nodes/mac.yml --log-level=DEBUG

0de0b68324b2ebe843c493e22d6a13f42e97f9a8
Takuma Homma <nagomimatcha@gmail.com>
2021-05-15 18:18:27 +0900

    Refactor spec

--- spec/mac/dotfiles_spec.rb
+++ spec/mac/dotfiles_spec.rb
@@ -4,13 +4,13 @@ describe file File.expand_path('src/github.com/dotfiles', '~') do
   it { is_expected.to be_directory }
 end
 
-%w(.vim .emacs.d .emacs.d/themes/material-theme .config/fish .config/fish/functions).each do |dir|
+%w(.vim .emacs.d .config/fish .config/fish/functions).each do |dir|
   describe file File.expand_path(dir, '~') do
     it { is_expected.to be_directory }
   end
 end
 
-%w(.gitconfig .vimrc .zshrc .tmux.conf .config/fish/config.fish).each do |file|
+%w(.gitconfig .vimrc .config/fish/config.fish).each do |file|
   describe file File.expand_path(file, '~') do
     it { is_expected.to be_file }
   end

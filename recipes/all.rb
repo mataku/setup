52bb81672065f3874c54472ead669cecc170c4a5
Takuma Homma <nagomimatcha@gmail.com>
2021-05-21 00:26:04 +0900

    Kick unused recipes

--- recipes/all.rb
+++ recipes/all.rb
@@ -3,7 +3,5 @@ include_recipe '../cookbooks/dotfiles/default'
 
 if `uname` == 'Darwin'
   include_recipe '../cookbooks/terminal/default'
-  include_recipe '../cookbooks/golang/default'
   include_recipe '../cookbooks/font/default'
-  include_recipe '../cookbooks/gui/default'
 end

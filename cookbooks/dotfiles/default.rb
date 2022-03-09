c7166d79538b5b5c5ecdaad4939a56d409bc5563
Takuma Homma <nagomimatcha@gmail.com>
2022-03-10 01:25:08 +0900

    Set correct path

--- cookbooks/dotfiles/default.rb
+++ cookbooks/dotfiles/default.rb
@@ -1,15 +1,15 @@
-%w(.config/coc/extensions src).each do |dir|
+%w(.config/coc/extensions src .config/mvim/rc).each do |dir|
   execute 'Create config dir' do
     command "mkdir -p ~/#{dir}"
   end
 end
 
-git File.expand_path('~/src/github.com/dotfiles') do
+git File.expand_path('~/src/github.com/mataku/dotfiles') do
   repository 'https://github.com/mataku/dotfiles.git'
 end
 
 execute 'Create symbolic links' do
-  command '/bin/bash ~/src/github.com/dotfiles/install.sh'
+  command '/bin/bash ~/src/github.com/mataku/dotfiles/install.sh'
 end
 
 execute 'Install fisher' do
@@ -18,7 +18,7 @@ execute 'Install fisher' do
 end
 
 execute 'Install bundletool' do
-  command 'curl -sLO https://github.com/google/bundletool/releases/download/0.13.4/bundletool-all.jar -o ~/src/github.com/dotfiles/bin/bundletool.jar'
+  command 'curl -sLO https://github.com/google/bundletool/releases/download/0.13.4/bundletool-all.jar -o ~/src/github.com/mataku/dotfiles/bin/bundletool.jar'
 end
 
 execute 'mkdir emacs themes dir' do
